b72b7c143f262a0b7852a7d2639a47868db5cdad
Umar Maqsood <you@example.com>
2022-11-04 10:22:04 +0500

    Minor Updates

--- src/Components/Pages/Portfolio/PortfolioWork.js
+++ src/Components/Pages/Portfolio/PortfolioWork.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Portfolio.css";
 import Aos from "aos";
 import { PortfolioApi } from "./PortfolioApi";
+import { useNavigate } from "react-router-dom";
 
 function PortfolioWork() {
   Aos.init();
   Aos.refresh();
+  const Navigate = useNavigate();
+  const [Noofallproducts, setNoofallProducts] = useState(9);
+  const sliceAllProducts = PortfolioApi.slice(0, Noofallproducts);
+  const loadMoreProducts = () => {
+    setNoofallProducts(Noofallproducts + Noofallproducts);
+  };
   return (
     <div id="portfolio-work">
       <h1 data-aos="zoom-in">
@@ -18,7 +25,7 @@ function PortfolioWork() {
         <button>Amazon</button>
       </div>
       <div className="container">
-        {PortfolioApi.map((data, index) => (
+        {sliceAllProducts.map((data, index) => (
           <div className="cards" data-aos="fade-up" key={index}>
             <img src={data.pic} alt="" />
             <div className="cap-box">
@@ -35,8 +42,8 @@ function PortfolioWork() {
         ))}
       </div>
       <div className="btnBox">
-        <button>See More</button>
-        <button>Let's Start</button>
+        <button onClick={loadMoreProducts}>See More</button>
+        <button onClick={() => Navigate("/order-now")}>Order Now</button>
       </div>
     </div>
   );
